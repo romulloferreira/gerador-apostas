86330ab3cb6552d80f2f2a36a12e74a8034c5fc8
Romullo Ferreira <romulloferreira@gmail.com>
2018-10-26 22:12:11 -0300

    commit projeto completo

---
+++ script_lotodificil.js
@@ -0,0 +1,29 @@
+var form = document.querySelector('form');
+var tbn_gerar = document.getElementById('submit');
+
+
+
+form.onsubmit = function(e) {
+         
+         document.getElementById('resultado').innerHTML = jogos;
+         e.preventDefault();
+
+}
+
+ javascript:(function(maxNum, numPorJogo, numJogos){
+	var jogos = [];
+	
+	while(jogos.length < numJogos){
+		var res = [];
+		
+		while(res.length < numPorJogo){
+			num = Math.floor((Math.random() * 25) + 1);
+			if(res.indexOf(num)==-1) res.push(num);
+		}
+		
+		jogos.push(res);
+	}
+	
+	document.getElementById('resultado').innerHTML = ("Boa sorte!", jogos.map(function(a){ return a.sort(function(a,b){return a-b;}).join(' '); }).join('\n'));
+})((25), (15), (1))
+	       
